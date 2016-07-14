0eeb34ff7071c2ba8829f39ae61d067b304e9bfa
thomaskim19 <97thomas@gmail.com>
2016-07-13 21:54:51 -0700

    Attempting to add viewPagers. Activity_Workout page not showing.

--- app/src/main/java/me/thomasdkim/fitit/workout/Workout.java
+++ app/src/main/java/me/thomasdkim/fitit/workout/Workout.java
@@ -1,36 +1,145 @@
 package me.thomasdkim.fitit.workout;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import me.thomasdkim.fitit.R;
 
 public class Workout extends AppCompatActivity {
+
+    ViewPager myView;
+    DateFragmentAdapter mDateFragmentAdapter;
+    DateFragment[] dateFragmentArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        String currentDate = DateFormat.getLongDateFormat(this).format(new Date());
+        /* Declare variablees  */
+        DateFragment[] dateFragList = new DateFragment[3];
+        Calendar yesterday, today, tomorrow;
+        yesterday = Calendar.getInstance();
+        today = Calendar.getInstance();
+        tomorrow = Calendar.getInstance();
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(50);
-        textView.setText(currentDate);
+        yesterday.add(Calendar.DAY_OF_MONTH, -1); //Set the date of yesterday to the day before
+        tomorrow.add(Calendar.DAY_OF_MONTH, 1); //Set the date of tomorrow to the day after
 
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.workoutscreen);
-        layout.addView(textView);
+
+        dateFragList[0] = DateFragment.newInstance(yesterday);
+        dateFragList[1] = DateFragment.newInstance(today);
+        dateFragList[2] = DateFragment.newInstance(tomorrow);
+
+
+
+
+        myView = (ViewPager) findViewById(R.id.pager);
+        mDateFragmentAdapter = new DateFragmentAdapter(getSupportFragmentManager(), dateFragList);
+        myView.setAdapter(mDateFragmentAdapter);
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myView.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            myView.setCurrentItem(myView.getCurrentItem() - 1);
+        }
+    }
+
+    public static class DateFragment extends Fragment {
+
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+        }
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.slide_date_fragment, container, true);
+            View newText = rootView.findViewById(R.id.datefrag);
+
+            if(getArguments().getString("date") != null) {
+                ((TextView)newText).setText(getArguments().getString("date"));
+            }
+
+            return rootView;
+        }
+
+        public static DateFragment newInstance(Calendar cal) {
+
+
+            //Create new date fragment object
+            DateFragment newFrag = new DateFragment();
+
+            //Create new Bundle to store aarguments
+            Bundle args = new Bundle();
+
+            //Set a "date" paramater the the object
+            args.putString("date", dateToString(cal));
+            newFrag.setArguments(args); //Set the argument to the newly created date fragment
+            return newFrag;
+        }
+
+
+    }
+
+    public static class DateFragmentAdapter extends FragmentStatePagerAdapter{
+
+        DateFragment[] fragList;
+        int fragNum = 3;
+
+        public DateFragmentAdapter(FragmentManager fm, DateFragment[] fragList) {
+            super(fm);
+            this.fragList = fragList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            return fragList[position];
+        }
+
+        @Override
+        public int getCount() {
+
+            return fragNum;
+        }
     }
 
 
-    public ArrayList<String> DataeList(){
+
+    public static ArrayList<String> DateList(){
 
         Calendar begindate,today,enddate;
         today =  Calendar.getInstance();
@@ -41,31 +150,32 @@ public class Workout extends AppCompatActivity {
         enddate = Calendar.getInstance();
 
         //Clear begindate and enddate
-        begindate.clear();
-        enddate.clear();
+//        begindate.clear();
+//        enddate.clear();
 
         //Set begindate and enddate to the current date
-        begindate.set(today.YEAR, today.MONTH, today.DAY_OF_MONTH);
-        enddate.set(today.YEAR, today.MONTH, today.DAY_OF_MONTH);
+//        begindate.set(today.YEAR, today.MONTH, today.DAY_OF_MONTH);
+//        enddate.set(today.YEAR, today.MONTH, today.DAY_OF_MONTH);
 
         //Make begindate yesterday, enddate tomorrow
         begindate.add(Calendar.DAY_OF_MONTH, -1);
         enddate.add(Calendar.DAY_OF_MONTH, 1);
 
-        //Checks that begindate is less than or equal to tomorrow
+        //Checks that begindate is less than or e   qual to tomorrow
         while(begindate.compareTo(enddate) <= 0){
             dateList.add(dateToString(begindate));
             begindate.add(Calendar.DAY_OF_MONTH, 1);
         }
 
+        dateList.add(dateToString(today));
 
         return dateList;
     }
 
-    public String dateToString(Calendar cal){
-        String dateString = new String();
+    public static String dateToString(Calendar cal){
+        String dateString;
         dateString = cal.get(Calendar.MONTH) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" +
-        "/" + cal.get(Calendar.YEAR);
+        + cal.get(Calendar.YEAR);
 
         return dateString;
     }
