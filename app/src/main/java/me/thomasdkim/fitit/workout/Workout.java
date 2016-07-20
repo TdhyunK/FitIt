8f8ada56e2551a64b47019e592d535ec55fcccb7
thomaskim19 <97thomas@gmail.com>
2016-07-19 21:39:11 -0700

    Working scrolling date. Implementing card UI for each exercise.

--- app/src/main/java/me/thomasdkim/fitit/workout/Workout.java
+++ app/src/main/java/me/thomasdkim/fitit/workout/Workout.java
@@ -7,19 +7,14 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.widget.TextView;
+
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -37,30 +32,85 @@ public class Workout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("The app is being created.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
         /* Declare variablees  */
-        DateFragment[] dateFragList = new DateFragment[3];
-        Calendar yesterday, today, tomorrow;
+        final Calendar yesterday, today, tomorrow;
         yesterday = Calendar.getInstance();
         today = Calendar.getInstance();
         tomorrow = Calendar.getInstance();
+        dateFragmentArray = new DateFragment[3];
+
+        yesterday.add(Calendar.MONTH, 1);
+        today.add(Calendar.MONTH, 1);
+        tomorrow.add(Calendar.MONTH, 1);
 
         yesterday.add(Calendar.DAY_OF_MONTH, -1); //Set the date of yesterday to the day before
         tomorrow.add(Calendar.DAY_OF_MONTH, 1); //Set the date of tomorrow to the day after
 
 
-        dateFragList[0] = DateFragment.newInstance(yesterday);
-        dateFragList[1] = DateFragment.newInstance(today);
-        dateFragList[2] = DateFragment.newInstance(tomorrow);
+        dateFragmentArray[0] = DateFragment.newInstance(yesterday);
+        dateFragmentArray[1] = DateFragment.newInstance(today);
+        dateFragmentArray[2] = DateFragment.newInstance(tomorrow);
 
 
+        System.out.println("The date is: " + dateToString(today));
 
 
         myView = (ViewPager) findViewById(R.id.pager);
-        mDateFragmentAdapter = new DateFragmentAdapter(getSupportFragmentManager(), dateFragList);
+        mDateFragmentAdapter = new DateFragmentAdapter(getSupportFragmentManager(), dateFragmentArray);
         myView.setAdapter(mDateFragmentAdapter);
+        myView.setCurrentItem(1);
+
+        myView.addOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if(state == ViewPager.SCROLL_STATE_IDLE){
+
+                    //Handles if the current page is tomorrow
+                    if(myView.getCurrentItem() == 2){
+                        yesterday.add(Calendar.DAY_OF_MONTH, 1);
+                        today.add(Calendar.DAY_OF_MONTH, 1);
+                        tomorrow.add(Calendar.DAY_OF_MONTH, 1);
+
+                        dateFragmentArray[0] = DateFragment.newInstance(yesterday);
+                        dateFragmentArray[1] = DateFragment.newInstance(today);
+                        dateFragmentArray[2] = DateFragment.newInstance(tomorrow);
+
+
+                    }
+
+                    //Handles if the current pagae is yesterday
+                    else if(myView.getCurrentItem() == 0){
+                        yesterday.add(Calendar.DAY_OF_MONTH, -1);
+                        today.add(Calendar.DAY_OF_MONTH, -1);
+                        tomorrow.add(Calendar.DAY_OF_MONTH, -1);
+
+                        dateFragmentArray[0] = DateFragment.newInstance(yesterday);
+                        dateFragmentArray[1] = DateFragment.newInstance(today);
+                        dateFragmentArray[2] = DateFragment.newInstance(tomorrow);
+
+                    }
+
+                    mDateFragmentAdapter = new DateFragmentAdapter(getSupportFragmentManager(), dateFragmentArray);
+                    myView.setAdapter(mDateFragmentAdapter);
+                    myView.setCurrentItem(1, false);
+
+                }
+            }
+        });
 
 
 
@@ -80,14 +130,11 @@ public class Workout extends AppCompatActivity {
 
     public static class DateFragment extends Fragment {
 
-        public void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-        }
-
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.slide_date_fragment, container, true);
-            View newText = rootView.findViewById(R.id.datefrag);
+
+            View rootView = inflater.inflate(R.layout.slide_date_fragment,container, false);
+            View newText = (TextView) rootView.findViewById(R.id.datefrag);
 
             if(getArguments().getString("date") != null) {
                 ((TextView)newText).setText(getArguments().getString("date"));
@@ -117,61 +164,29 @@ public class Workout extends AppCompatActivity {
     public static class DateFragmentAdapter extends FragmentStatePagerAdapter{
 
         DateFragment[] fragList;
-        int fragNum = 3;
+        int fragNum;
 
         public DateFragmentAdapter(FragmentManager fm, DateFragment[] fragList) {
             super(fm);
+            this.fragNum = 3;
             this.fragList = fragList;
         }
 
         @Override
-        public Fragment getItem(int position) {
+        public DateFragment getItem(int position) {
 
-            return fragList[position];
+            return this.fragList[position];
         }
 
         @Override
         public int getCount() {
 
-            return fragNum;
+            return this.fragNum;
         }
     }
 
 
-
-    public static ArrayList<String> DateList(){
-
-        Calendar begindate,today,enddate;
-        today =  Calendar.getInstance();
-        ArrayList<String> dateList = new ArrayList<String>();
-
-        //Initialize begindate and enddate
-        begindate = Calendar.getInstance();
-        enddate = Calendar.getInstance();
-
-        //Clear begindate and enddate
-//        begindate.clear();
-//        enddate.clear();
-
-        //Set begindate and enddate to the current date
-//        begindate.set(today.YEAR, today.MONTH, today.DAY_OF_MONTH);
-//        enddate.set(today.YEAR, today.MONTH, today.DAY_OF_MONTH);
-
-        //Make begindate yesterday, enddate tomorrow
-        begindate.add(Calendar.DAY_OF_MONTH, -1);
-        enddate.add(Calendar.DAY_OF_MONTH, 1);
-
-        //Checks that begindate is less than or e   qual to tomorrow
-        while(begindate.compareTo(enddate) <= 0){
-            dateList.add(dateToString(begindate));
-            begindate.add(Calendar.DAY_OF_MONTH, 1);
-        }
-
-        dateList.add(dateToString(today));
-
-        return dateList;
-    }
-
+    //Create a date string from a calendar object
     public static String dateToString(Calendar cal){
         String dateString;
         dateString = cal.get(Calendar.MONTH) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" +
@@ -180,7 +195,8 @@ public class Workout extends AppCompatActivity {
         return dateString;
     }
 
+    public static void updateDateList(DateFragment[] dateList){
 
-
+    }
 
 }
